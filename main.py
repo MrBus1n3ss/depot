8173a9479c3fce8f2157a800ee9f30fc69263213
John Richardson <john.richardson@jvrdev.com>
2024-12-26 10:07:41 -0600

    created boiler plate code

---
+++ main.py
@@ -0,0 +1,58 @@
+class FileSystem:
+    def __init__(self):
+        pass
+
+    def create_file_system(self):
+        pass
+
+    def update_file_system(self):
+        pass
+
+    def get_file_system(self):
+        pass
+
+    def delete_file_system(self):
+        pass
+
+
+class Dir:
+    def __init__(self):
+        pass
+
+    def create_dir(self):
+        pass
+
+    def update_dir(self):
+        pass
+
+    def get_dir(self):
+        pass
+
+    def delete_dir(self):
+        pass
+
+
+class File:
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+
+    def create_file(self):
+        pass
+
+    def update_file(self):
+        pass
+
+    def get_file(self):
+        pass
+
+    def delete_file(self):
+        pass
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
