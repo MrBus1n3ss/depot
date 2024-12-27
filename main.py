3db3f9842d6445581b0b047aa952f8c475680db3
John Richardson <john.richardson@jvrdev.com>
2024-12-27 02:59:11 -0600

    starting figuring out what I want to make

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-class FileSystem:
-    def __init__(self):
-        pass
+import os
+from pathlib import Path
+
+
+class FileSystem(object):
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(FileSystem, cls).__new__(cls)
+        return cls.instance
+
+    def config(self, project, location):
+        self.project = project
+        self.location = location
 
     def create_file_system(self):
+        try:
+            print(self.project)
+            print(self.location)
+        except AttributeError:
+            print('Please run config First')
+        except Exception:
+            print('Unknown Error')
+
+    def update_file_system(self, location):
         pass
 
-    def update_file_system(self):
+    def get_file_system(self, location):
         pass
 
-    def get_file_system(self):
-        pass
-
-    def delete_file_system(self):
+    def delete_file_system(self, location):
         pass
 
 
@@ -33,14 +50,20 @@ class Dir:
 
 
 class File:
-    def __init__(self, name, location):
+    def __init__(self, name: str, location: list):
         self.name = name
         self.location = location
+        self.project_path = Path.cwd()
 
     def create_file(self):
-        pass
+        file_dir = os.path.join(Path.cwd(), self.location)
+        print(file_dir)
+        # print(Path(self.project_path / self.location))
+        # print(Path.is_dir(self.project_path / self.location))
+        # print(self.location)
+        # print(self.project_path)
 
-    def update_file(self):
+    def rename_file(self, name):
         pass
 
     def get_file(self):
@@ -49,9 +72,19 @@ class File:
     def delete_file(self):
         pass
 
+    def write_to_file(self, data):
+        pass
 
+
+# TODO: going to make this more like buckets, need to make into server
 def main():
-    print("Hello World!")
+    # file_system = FileSystem()
+    # file_system.config('data', '~/test')
+    # file_system.create_file_system()
+    file = File('test', '/data')
+    new_file = File('test2', '/data/test')
+    file.create_file()
+    new_file.create_file()
 
 
 if __name__ == "__main__":
