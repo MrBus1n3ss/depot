dc8103ad33f147654e8df6f7aa4927376a722f5b
John Richardson <john.richardson@jvrdev.com>
2025-01-07 14:54:42 -0600

    added ability to save dir

--- main.py
+++ main.py
@@ -19,38 +19,95 @@ class DB:
     def get_cursor(self):
         return self.connection.cursor()
 
+    def close_connection(self):
+        self.connection.close()
 
-class DepotSystem:
-    def __init__(self, name, db):
-        self.name = name
+
+# Row name:str depot:str dirs:list date_created:timestamp
+# TODO: create dir for depots
+class Depots:
+    def __init__(self, db):
         self.db = db
-        self.created_at = datetime.now()
         self.cursor = self.db.get_cursor()
+        self.cursor.execute("""
+            create table if not exists depots
+            (
+                name text not null unique,
+                created_at text,
+                created_by text,
+                modify_at text,
+                modify_by text
+            );
+            """)
+        self.cursor.execute("""
+            create table if not exists dirs
+            (
+                dir_name text not null,
+                depot_name text not null,
+                created_at text,
+                created_by text,
+                modify_at text,
+                modify_by text,
+                foreign key(depot_name) references depots(name)
+                unique(dir_name, depot_name)
+            );
+            """)
+        self.db.connection.commit()
 
-    def create_depot(self, depot_name):
-        row = self.cursor.execute("select name, hash from depot_mapping").fetchone()
-        if row is None:
-            hash = self.create_hash()
-            self.cursor.execute("insert into depot_mapping values (?, ?)", (depot_name, hash,))
-            print(self.cursor.execute("select name, hash from depot_mapping").fetchall())
-            self.db.connection.commit()
+    def get_depot(self, name):
+        row = self.cursor.execute("select * from depots where name = ?", (name,)).fetchone()
+        if not row:
+            raise Exception(f'{name} does not exist')
+        if row[0] == name:
+            return Depot(row[0], row[1], row[2], row[3])
 
-        else:
-            raise Exception(f'{depot_name} already exists')
+    def create_depot(self, depot):
+        self.cursor.execute("insert into depots values (?, ?, ?, ?, ?)",
+                                (depot.name,
+                                 depot.created_at,
+                                 depot.created_by,
+                                 depot.modify_by,
+                                 depot.modify_at))
+        self.db.connection.commit()
 
-    def create_hash(self):
-        hash = hashlib.sha256()
-        hash.update(bytes(self.name.encode('utf-8')))
-        hash.update(bytes(str(self.created_at).encode('utf-8')))
-        return hash.hexdigest()
-
+    def delete_depot(self, depot):
+        pass
 
 
 class Depot:
-    def __init__(self, name, created_at, db):
+    def __init__(self,
+                 name: str,
+                 dirs: list,
+                 created_by: str,
+                 modify_by: str):
+        now = datetime.now()  # TODO: will need to change how this works
         self.name = name
-        self.created_at = created_at
-        self.db = db
+        self.dirs = dirs
+        self.created_at = now
+        self.created_by = created_by
+        self.modify_at = now
+        self.modify_by = modify_by
+
+    def create_dir(self, db, dir_name):  # TODO: figure out how I want to do the db part
+        cursor = db.get_cursor()
+        cursor.execute("insert into dirs values(?, ?, ?, ?, ?, ?)",
+                       (dir_name,
+                        self.name,
+                        self.created_at,
+                        self.created_by,
+                        self.modify_at,
+                        self.modify_by))
+        db.connection.commit()
+
+
+    def get_dir_tree(self):
+        pass
+
+    def get_dirs(self):
+        pass
+
+    def get_dir(self):
+        pass
 
     def store_file(self):
         pass
@@ -71,13 +128,37 @@ class Depot:
         pass
 
 
+def filename_parser(filename):
+    return filename.split(':')
+
+
+def create_test_dir(depot_mapping, depot):
+    depot.create_dir(depot_mapping, 'test')
+
+
+def create_test_depot(depots):
+    name = 'test'
+    depot = Depot(name, [], 'jrichardson12', 'jrichardson12')
+    depots.create_depot(depot)
+
+
+def get_depots():
+    depot_mapping = DB(data_dir, "depots.db")
+    depot_mapping.connection.commit()
+    return Depots(depot_mapping), depot_mapping
+
+
+def get_test_depot(depots):
+    return depots.get_depot('test')
+
+
 # TODO: going to make this more like buckets, need to make into server
 def main():
-    depot_mapping = DB(data_dir, "depot_mapping.db")
-    depot_mapping.get_cursor().execute("create table if not exists depot_mapping (name text, hash text)")
-    depot_mapping.connection.commit()
-    depot_system = DepotSystem('test', depot_mapping)
-    depot_system.create_depot('test')
+    depots, depot_mapping = get_depots()
+    # create_test_depot(depots)
+    depot = get_test_depot(depots)
+    create_test_dir(depot_mapping, depot)
+
 
 
 if __name__ == "__main__":
