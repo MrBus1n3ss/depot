77b9be2ac39dad61035d5d671b8b5b54819617cf
John Richardson <john.richardson@jvrdev.com>
2025-01-08 11:31:50 -0600

    updated dir tables

--- main.py
+++ main.py
@@ -43,13 +43,14 @@ class Depots:
             create table if not exists dirs
             (
                 dir_name text not null,
+                parent_dir text,
                 depot_name text not null,
                 created_at text,
                 created_by text,
                 modify_at text,
                 modify_by text,
                 foreign key(depot_name) references depots(name)
-                unique(dir_name, depot_name)
+                unique(dir_name, depot_name, parent_dir)
             );
             """)
         self.db.connection.commit()
@@ -70,7 +71,7 @@ class Depots:
                                  depot.modify_at))
         self.db.connection.commit()
 
-    def delete_depot(self, depot):
+    def delete_depot(self, depot):  # TODO cascade delete with deleting all files 
         pass
 
 
@@ -88,10 +89,12 @@ class Depot:
         self.modify_at = now
         self.modify_by = modify_by
 
-    def create_dir(self, db, dir_name):  # TODO: figure out how I want to do the db part
+    def create_dir(self, db, dir_name, parent_dir='root'):  # TODO: figure out how I want to do the db part
         cursor = db.get_cursor()
-        cursor.execute("insert into dirs values(?, ?, ?, ?, ?, ?)",
+        # TODO: validate dir name
+        cursor.execute("insert into dirs values(?, ?, ?, ?, ?, ?, ?)",
                        (dir_name,
+                        parent_dir,
                         self.name,
                         self.created_at,
                         self.created_by,
@@ -99,12 +102,15 @@ class Depot:
                         self.modify_by))
         db.connection.commit()
 
-
     def get_dir_tree(self):
         pass
 
-    def get_dirs(self):
-        pass
+    def get_dirs(self, db):
+        cursor = db.get_cursor()
+        rows = cursor.execute("select * from dirs where depot_name = ?",
+                              (self.name,)).fetchall()
+        return rows
+
 
     def get_dir(self):
         pass
@@ -131,9 +137,14 @@ class Depot:
 def filename_parser(filename):
     return filename.split(':')
 
-
+# These are for testing ========================================================
 def create_test_dir(depot_mapping, depot):
-    depot.create_dir(depot_mapping, 'test')
+    depot.create_dir(depot_mapping, 'test_dir')
+
+
+def create_parent_dir(depot_mapping, depot):
+    depot.create_dir(depot_mapping, 'parent_dir')
+    depot.create_dir(depot_mapping, 'child_dir', 'parent_dir')
 
 
 def create_test_depot(depots):
@@ -150,15 +161,17 @@ def get_depots():
 
 def get_test_depot(depots):
     return depots.get_depot('test')
+# ==============================================================================
 
 
 # TODO: going to make this more like buckets, need to make into server
 def main():
     depots, depot_mapping = get_depots()
-    # create_test_depot(depots)
+    create_test_depot(depots)
     depot = get_test_depot(depots)
     create_test_dir(depot_mapping, depot)
-
+    create_parent_dir(depot_mapping, depot)
+    print(depot.get_dirs(depot_mapping))
 
 
 if __name__ == "__main__":
